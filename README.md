5136c8957cb8d5b15747d3bd587b47a7ceaa1997
Barrior <barrior@qq.com>
2021-01-04 21:34:29 +0800

    feat(eslint-ts): the lint specification
    - support TypeScript with ESLint.
    - use prettier format code style.
    - use commitlint check `git commit`.
    - support to add `.gitignore` file.

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
 # lint-files
-通过一条命令为前端项目引入统一的规范，类似 dotfiles
+通过一条命令为前端项目引入统一的规范，类似 dotfiles。
+
+## 使用
+
+在项目根目录执行下面命令，就会以 `lint-files/eslint-ts` 的规范为项目安装所需依赖及配置。
+
+```bash
+curl -sL https://raw.githubusercontent.com/Barrior/lint-files/main/eslint-ts/install.sh | bash
+```
+
+命令允许加入参数，如下示例，使用 `npm` 替换默认的 `yarn` 包管理工具进行所需依赖的安装。
+
+```bash
+curl -sL https://raw.githubusercontent.com/Barrior/lint-files/main/eslint-ts/install.sh | bash -s -- --use-npm
+```
+
+## 参数列表
+
+- `--use-npm`: 使用 npm 安装，默认使用 yarn 安装。
+- `--add-gitignore`: 添加 `.gitignore` 规范文件。
+- `--add-tsconfig`: 添加 `tsconfig.json` 规范文件。
+
+## FAQ
+
+如果命令没有执行成功，有可能是 GitHub 的 DNS 被污染了，修正 host 即可，如下。
+
+```bash
+199.232.68.133 raw.githubusercontent.com
+```
---
+++ eslint-ts/install.sh
@@ -0,0 +1,86 @@
+#!/bin/bash
+
+# Declare variable
+ADD_GIT_IGNORE=0
+ADD_TSCONFIG=0
+INSTALL_CMD='yarn add -D'
+ORIGIN_DIR='https://raw.githubusercontent.com/Barrior/lint-files/main/eslint-ts'
+DEPENDENCIES='
+@commitlint/cli
+@commitlint/config-conventional
+
+@typescript-eslint/eslint-plugin
+@typescript-eslint/parser
+
+eslint
+eslint-config-prettier
+eslint-plugin-import
+eslint-plugin-prettier
+eslint-plugin-simple-import-sort
+eslint-plugin-unused-imports
+
+husky
+lint-staged
+prettier
+stylelint
+typescript
+'
+COMMIT_FILES='
+.editorconfig
+.eslintrc
+.prettierrc
+commitlint.config.js
+package.json
+'
+
+for arg in "$@"; do
+  if [ $arg = '--use-npm' ]; then
+    INSTALL_CMD='npm i -D'
+  fi
+
+  if [ $arg = '--add-gitignore' ]; then
+    ADD_GIT_IGNORE=1
+  fi
+
+  if [ $arg = '--add-tsconfig' ]; then
+    ADD_TSCONFIG=1
+  fi
+done
+
+# Install lint dependencies
+eval $INSTALL_CMD $DEPENDENCIES --ignore-workspace-root-check
+
+# Download lint files
+curl -sLO "$ORIGIN_DIR/.editorconfig" \
+  -sLO "$ORIGIN_DIR/.eslintrc" \
+  -sLO "$ORIGIN_DIR/.prettierrc" \
+  -sLO "$ORIGIN_DIR/commitlint.config.js"
+
+if [ $ADD_GIT_IGNORE = 1 ]; then
+  curl -sLO "$ORIGIN_DIR/.gitignore"
+  COMMIT_FILES=$COMMIT_FILES' .gitignore'
+fi
+
+if [ $ADD_TSCONFIG = 1 ]; then
+  curl -sLO "$ORIGIN_DIR/tsconfig.json"
+  COMMIT_FILES=$COMMIT_FILES' tsconfig.json'
+fi
+
+# Get and adjust lint json
+PACKAGE_LINT_JSON=$(
+  curl -sL "$ORIGIN_DIR/package-lint.json" |
+  # Trim "{" in first line and "}" in last line
+  sed -e '1a\'$'\n'',' -e '1d' -e '$d' |
+  # Delete blank space like newline, tab and so on.
+  tr -d '\n\r\f\t\v'
+)
+
+# Append lint json to the end of package.json
+sed -i '' -e '$a\'$'\n'"$PACKAGE_LINT_JSON""}" -e '$d' package.json
+
+# Pretty package.json
+npx prettier --write package.json
+
+# Commit changes
+git add $COMMIT_FILES
+git commit -m "chore(lint): add lint tools" $COMMIT_FILES
